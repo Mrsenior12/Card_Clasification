2022be6e164f76070d7d148f639bac08480cf22c
Mrsenior12 <61933232+Mrsenior12@users.noreply.github.com>
2021-11-18 13:39:46 +0100

    dodanie bibliotek

--- Projekt-Obrazy/Obrazy.py
+++ Projekt-Obrazy/Obrazy.py
@@ -1,1 +1,12 @@
-print("test")
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import numpy as np
+import pandas as pd
+import cv2
+import os
+from random import shuffle
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+
